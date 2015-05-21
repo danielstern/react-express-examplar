174f188478ac879d8f3dfceef1e12ee1e4f7f431
Daniel Stern <daniel@danielstern.ca>
2015-05-20 22:00:41 -0400

    react js repeater implemented!

--- app/components/GroceryItemList.jsx
+++ app/components/GroceryItemList.jsx
@@ -4,11 +4,29 @@ let GroceryItem = require('./GroceryItem.jsx');
 /*let GroceryItem = require('./components/GroceryItem.jsx');*/
 
 module.exports = React.createClass({
+	getInitialState:function(){
+		return {
+			items:[{
+				name:"Oats",
+				purchased:false,
+				price:3.00,				
+			},{
+				name:"Milk",
+				purchased:true,
+				price:7.00,				
+			}]
+		}
+	},
 	render:function(){
 		return (
 			<div>
 				Grocery Items On Your List
-				<GroceryItem name="Oats" />
+				{this.state.items.map(function(item,index){
+					return (
+						<GroceryItem name={item.name} key={"item"+index} />		
+					)
+				})}
+				
 			</div>
 		)
 	}
