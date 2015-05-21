f2ac6564d6fa6643563983494c03bbda66fc7b40
Daniel Stern <daniel@danielstern.ca>
2015-05-21 12:32:56 -0400

    implemented a mongo bootloader

--- server/main.js
+++ server/main.js
@@ -1,9 +1,34 @@
 "use strict";
 
-var Express = require('express');
+let Express = require('express');
+let cors = require('cors');
+let mongoose = require('mongoose');
+
+let groceryItemSchema = {
+	name:String,
+	id:String,
+	cost:Number
+};
+
+
+let GroceryItem = mongoose.model('GroceryItem',groceryItemSchema,'groceryItems');
+
+mongoose.connect('mongodb://localhost/grocery',function(){
+	mongoose.connection.db.dropDatabase();
+
+	[{name:"Ice Cream"},{name:"Waffles"}].forEach(function(item){
+		new GroceryItem({
+			name:item.name
+		}).save();
+	})
+});
+
+
+
+
 
 let backend = new Express();
-
+backend.use(cors());
 backend.get('/',function(req,res){
 	res.json({
 		version:'version/',
@@ -13,11 +38,17 @@ backend.get('/',function(req,res){
 backend.get('version/',function(req,res){
 	res.json('1.0.2')
 });
+
+backend.get('/items',function(req,res){
+	GroceryItem.find(function(error,doc){
+		res.send(doc);
+	})
+})
  
 backend.listen(7777);
 
+
+
 let frontend = new Express();
-
 frontend.use(Express.static(__dirname + '/../.tmp'))
-
 frontend.listen(80);
