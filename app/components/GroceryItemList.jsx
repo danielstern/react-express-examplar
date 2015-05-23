09d5d40fe736981a7b64f3ed2b31dbad95406360
Daniel Stern <daniel@danielstern.ca>
2015-05-23 12:06:18 -0400

    Got isomorphic rendering by moving app data fetching to the app level. Very few changes required. Isomorph looks right but doesnt actually do anything

--- app/components/GroceryItemList.jsx
+++ app/components/GroceryItemList.jsx
@@ -2,25 +2,15 @@
 
 let GroceryItem = require('./GroceryItem.jsx'),
 	GroceryListAddItem = require('./GroceryListAddItem.jsx'),
-	React = require('react/addons'),
-	GroceryItemStore = require('./../stores/GroceryItemStore.jsx');
+	React = require('react/addons');
 
 module.exports = React.createClass({
-	getInitialState:function(){
-		return {
-			items:GroceryItemStore.getGroceryItems()
-		}
-	},
-	componentDidMount:function(){
-		GroceryItemStore.onChange(()=>{
-			this.setState({items:GroceryItemStore.getGroceryItems()})
-		})
-	},
+	
 	render:function(){
 		return (
 			<div>
 				Grocery Items On Your List
-				{this.state.items.map((item,index)=>{
+				{this.props.items.map((item,index)=>{
 					return (
 						<GroceryItem item={item} key={"item"+index} />		
 					)
--- app/components/GroceryListApp.jsx
+++ app/components/GroceryListApp.jsx
@@ -1,12 +1,23 @@
 let GroceryItemList = require('./GroceryItemList.jsx');
 let React = require('react/addons');
+let GroceryItemStore = require('./../stores/GroceryItemStore.jsx');
 
 module.exports = React.createClass({
+	getInitialState:function(){
+		return {
+			items:GroceryItemStore.getGroceryItems()
+		}
+	},
+	componentDidMount:function(){
+		GroceryItemStore.onChange(()=>{
+			this.setState({items:GroceryItemStore.getGroceryItems()})
+		})
+	},
 	render:function(){
 		return (
 			<div>
 				<h1>Grocery Listify</h1>
-				<GroceryItemList/>
+				<GroceryItemList items={this.state.items}/>
 			</div>
 		)
 	}
