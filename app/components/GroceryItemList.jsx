700a3f974b6130c2bca8ba1755a9d161ca633d18
Code Whisperer <daniel@danielstern.ca>
2015-07-10 08:00:13 -0400

    complete add groceryitem

--- app/components/GroceryItemList.jsx
+++ app/components/GroceryItemList.jsx
@@ -5,6 +5,14 @@ module.exports = React.createClass({
         return (
             <div>
                 <h1>Grocery Listify </h1>
+                <div>
+                    {this.props.items.map(function(item,index){
+                        return (
+                            <div>{item.name}</div>
+                        )
+                    })                       
+                    }
+                </div>
             </div>
         )
     }
--- app/main.jsx
+++ app/main.jsx
@@ -5,4 +5,15 @@ console.log("Hello from JSX!");
 
 var GroceryItemList = require('./components/GroceryItemList.jsx');
 
-React.render(<GroceryItemList />,app)
+var initial = [{
+	name:"Ice Cream"
+},{
+	name:"Waffles"
+},{
+	name:"Candy",
+	purchased:true
+},{
+	name:"Snarks"
+}];
+
+React.render(<GroceryItemList items={initial}/>,app)
