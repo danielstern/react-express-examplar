86899e4fdc7fb02018ccbaf7ce2ca0a5c3358e93
Code Whisperer <daniel@danielstern.ca>
2015-07-28 15:08:19 -0400

    isomorph complete

--- server/main.js
+++ server/main.js
@@ -4,10 +4,25 @@ var app = new express();
 
 var parser = require('body-parser');
 
+var React = require('react/addons');
+
+var GroceryItem = require('./models/GroceryItem.js');
+
+require('babel/register');
 require('./database.js');
 
 app.get('/',function(req,res){
-    res.render('./../app/index.ejs',{});
+    //res.render('./../app/index.ejs',{});
+    var application = React.createFactory(require('./../app/components/GroceryItemList.jsx'));
+    
+    GroceryItem.find(function(error,doc){
+        var generated = React.renderToString(application({
+            items:doc
+        }));
+        
+        res.render('./../app/index.ejs',{reactOutput:generated});
+    })
+    
 })
 .use(express.static(__dirname + '/../.tmp'))
 .listen(7777);
